50eb5dcbe43f41789406288342f7f49085b9f065
Swati Yadav <808284siddhiksha@gmail.com>
2024-04-27 13:07:40 +0530

    done

---
+++ calculator.js
@@ -0,0 +1,17 @@
+export let sumData=(a,b)=>{
+    return a+b; 
+}
+export let minData=(a,b)=>{
+    return a-b; 
+}
+
+//export {sumData , minData}
+
+// export function sumData(a, b) {
+//     return a + b;
+// }
+// export function minData(a, b) {
+//     return a - b;
+// }
+
+export let cName ="SwatiYadav"
---
+++ index.js
@@ -0,0 +1,6 @@
+import {sumData as sum , minData , cName} from "./calculator.js";
+
+
+console.log(sum(40,20))
+console.log(minData(40,20))
+console.log(cName)
